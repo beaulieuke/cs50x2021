89f27e6d7a2b98282f008e495e013602b2fcddb4
Kevin Beaulieu <83260131+beaulieuke@users.noreply.github.com>
2021-12-06 16:06:50 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# cs50x-2021
+# this is my lab files for cs50 that was taken in 2021
+# i just learned how to use github so thankyou for your patience
---
+++ week1/lab1/hello/hello.c
@@ -0,0 +1,10 @@
+ #Prompt a message for user' name, and greeting message with user's name.
+*/
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string name = get_string("What is your name?\n");
+    printf("hello, %s\n", name);
+}
